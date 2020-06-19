e9121206ce29956d5d286c12c96685fa107944ef
Beatriz Martins <beatrizmartinsrt@gmail.com>
2020-06-19 16:39:11 -0300

    primeiro commit

---
+++ ListaTarefas/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { TaskListItemComponent } from './tasks/task-list-item/task-list-item.component';
+import { TaskFormComponent } from './tasks/task-form/task-form.component';
+import { TaskComponent } from './tasks/shared/task/task.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskListComponent,
+    TaskListItemComponent,
+    TaskFormComponent,
+    TaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
